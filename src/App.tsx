de8878227e7b56f3e47420584b046a59639d93d8
Pubudu Janith <pubudujanith123@gmail.com>
2023-04-10 18:50:17 +0530

    Add routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./pages/Login";
+import SharedLayout from "./pages/SharedLayout";
+import Dashboard from "./pages/Dashboard";
+import SaveStudent from "./pages/SaveStudent";
+import GetStudent from "./pages/GetStudent";
+import UpdateStudent from "./pages/UpdateStudent";
+import DeleteStudent from "./pages/DeleteStudent";
 
 function App() {
+  // Browse router is used to connect to the browser.
+  // Routes is the parent component for all Route components.
+  // Route is used to navigate to every page and has 2 props path and element.
+  // In the index route, Index is exactly matched to the parent component url.
   return (
-    <div>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Login/>}/>
+          <Route path={"dashboard"} element={<SharedLayout/>}>
+            <Route index element={<Dashboard/>}/>
+            <Route path={"save"} element={<SaveStudent/>}/>
+            <Route path={"get"} element={<GetStudent/>}/>
+            <Route path={"update"} element={<UpdateStudent/>}/>
+            <Route path={"delete"} element={<DeleteStudent/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
